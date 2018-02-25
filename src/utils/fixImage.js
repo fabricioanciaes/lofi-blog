82e9b015829f44ce9eedb8e503e32630272c34e0
Fabricio Anciães <Fabricio4p93@gmail.com>
2018-02-25 15:25:50 -0300

    ajuste pequeno na gambiarra do path

--- src/utils/fixImage.js
+++ src/utils/fixImage.js
@@ -1,6 +1,6 @@
 export default function fixImage(path) {
     if (!path) return null;
-    const pathPrefix = "/static/";
+    const pathPrefix = "/static";
 
     if(path.startsWith(pathPrefix)) {
         let newPath = path.slice(pathPrefix.length);
